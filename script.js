da9a020c15f667b28933e7aae748916af741ae67
Navyasree mommineedi <mnsd1385@gmail.com>
2026-06-21 18:36:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector("nav");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
